adbc802ca16e3f59d31f55099ac63a4c313a21dd
kokorink <kokorin.konstantin@gmail.com>
2018-01-20 20:43:42 +0300

    1st	commit

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DomainModelTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DomainModelTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91e6c988-69a8-411b-9914-c28908c1f583")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/SuggestionRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DomainModelTests
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        [TestMethod]
+        public void GetTest()
+        {
+            SuggestionRepository r = new SuggestionRepository();
+            Suggestion x = new Suggestion();
+            //x.Id = 1; само добавиться))
+            x.Subject = "СУКААААААА";
+            x.Date = new DateTime(2015, 7, 20);//обязательное поле по бд
+            //Arrange
+            
+            r.Create(x);
+            r.Save();
+            //Act
+            var result = r.GetSuggestionList();
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(List<Suggestion>));
+            r.Delete(x.Id);
+            r.Save();
+        }
+        [TestMethod]
+        public void CreateTest()
+        {
+            SuggestionRepository r = new SuggestionRepository();
+            //Arrange
+            Suggestion x = new Suggestion();
+            //Act
+            x.Subject = "СУКААААААА";
+            x.Date = new DateTime(2015, 7, 20);//обязательное поле по бд
+            r.Create(x);
+            r.Save();
+            //Assert
+            var get = r.GetSuggestionList();
+            Suggestion sub = get.FirstOrDefault(equal => equal.Subject == "СУКААААААА");
+            Assert.AreEqual("СУКААААААА", sub.Subject);
+            r.Delete(x.Id);
+            r.Save();
+
+        }
+    }
+}
